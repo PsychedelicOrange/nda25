ff37413cadd813091ab7a42c084350b1286883ca
Nikhil Sule <nikhil.atul.sule@gmail.com>
2025-05-18 16:40:17 +0530

    Day 1 dialogue test
    moved some nodes around in the scene for a bit of Day1 dialogue testing
    Made the .dtl for Day 1

---
+++ Scenes/Actual Scenes/DialogueController.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var window:bool = false
+
+func _ready() -> void:
+	pass
+
+func run_dialogue(dialogue_string:String):
+	Dialogic.start(dialogue_string)
+
+func _on_plant_pressed() -> void:
+	run_dialogue("Day1Opening")
+
+func _on_window_pressed() -> void:
+	Dialogic.VAR.Day1.set("window", true)
+	print(Dialogic.VAR.Day1.window)
+	pass
